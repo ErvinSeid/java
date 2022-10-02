edaa8e491b50963f823a36220cd5f187e1fe3d50
ErvinSeid <96637568+ErvinSeid@users.noreply.github.com>
2022-10-02 16:24:37 +0300

    Add files via upload

---
+++ Secondtest.java
@@ -0,0 +1,18 @@
+package test_project;
+
+public class Secondtest {
+
+    public static void main(String[] args) {
+        // TODO Auto=generated method stub
+
+        int num = 50;
+
+        if (num > 0) {
+            System.out.println("Positive number");
+        } else if (num < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
