c469803be44a0a3a47d9b3ab1f2dc201d4f8e230
Ervin Seid <96637568+ErvinSeid@users.noreply.github.com>
2023-02-06 00:52:29 +0200

    Add files via upload

---
+++ P02WorkingDay.java
@@ -0,0 +1,27 @@
+package L03VerConditionalStatementsAdvanced;
+
+import java.util.Scanner;
+
+public class P02WorkingDay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String dayOfWeek = scanner.nextLine();
+
+        switch (dayOfWeek) {
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+            System.out.println("Working day");
+                break;
+            case "Saturday":
+            case "Sunday":
+            System.out.println("Weekend");
+                break;
+            default:
+                System.out.println("Error");
+        }
+    }
+}
